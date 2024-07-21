ffc6fe88ef333198e63bccc904e08a3ae84a2935
kudy <bayardkudy1@gmail.com>
2024-07-21 12:49:42 +0100

    Create Game class and modified Point class

--- Game.cs
+++ Game.cs
@@ -8,10 +8,11 @@ namespace CleaningGame2
 {
     internal class Game
     {
-        public Robot Rob {  get; set; }
+        public Robot Rob {  get; set; } 
         public Point BottomRight { get; set; }
         public int NumObstacles { get; set; }
         public int NumDirts { get; set; }
+        public char[,] Board { get; set; }
 
         public Game() { }
         public Game(Point bottomRight, int numObstacles, int numDirts)
@@ -19,6 +20,80 @@ namespace CleaningGame2
             BottomRight = bottomRight;
             NumObstacles = numObstacles;
             NumDirts = numDirts;
+            Rob = new Robot()
+            {
+                TopLeft = new Point(),
+                BottomRight = bottomRight,
+                //Placing the robot in the middle of the board
+                CurrentPosition = new Point(bottomRight.X/2, bottomRight.Y/2),
+                Name = 'R'
+            };
+        }
+
+        public void InitBoard()
+        {
+            Board = new char[BottomRight.X, BottomRight.Y];
+            for (int i = 0; i < BottomRight.X; i++)
+                for (int j = 0; j < BottomRight.Y; j++)
+                    Board[i, j] = ' ';
+        }
+         public void PlaceRobot()
+        {
+            Board[Rob.CurrentPosition.X, Rob.CurrentPosition.Y] = Rob.Name;
+        }
+
+        public Point RandomPosition()
+        {
+            Point p = new Point();
+            Random r = new Random();
+            p.X = r.Next(BottomRight.X);
+            p.Y = r.Next(BottomRight.Y);
+
+            return p;
+        }
+
+        public void PlaceItems()
+        {
+            for (int i = 0; i < NumObstacles; i++)
+            {
+                Item obs = new Item()
+                {
+                    Name = 'O',
+                    Position = RandomPosition(),
+
+                };
+                while (Board[obs.Position.X, obs.Position.Y] != ' ')
+                obs.Position = RandomPosition();
+
+            }
+
+            for (int i = 0; i < NumDirts; i++)
+            {
+                Item dirt = new Item()
+                {
+                    Name = 'D',
+                    Position = RandomPosition(),
+                };
+                while (Board[dirt.Position.X, dirt.Position.Y] != ' ')
+                    dirt.Position = RandomPosition();
+            }
+        }
+
+        public void InitGame()
+        {
+            InitBoard();
+            PlaceRobot();
+            PlaceItems();
+        }
+
+        public void DisplayBoard()
+        {
+            for (int i = 0; i < BottomRight.X; i++)
+            {
+                for (int j = 0; j < BottomRight.Y; j++)
+                    Console.WriteLine(Board[i, j]); 
+                Console.WriteLine();
+            }
         }
     }
 }
--- Point.cs
+++ Point.cs
@@ -11,6 +11,14 @@ namespace CleaningGame2
         public int X {  get; set; } 
         public int Y { get; set; }
 
-        public Point() { }
+        public Point() {
+            X = 0;
+            Y = 0;
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
